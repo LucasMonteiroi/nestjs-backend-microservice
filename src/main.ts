24c4232dec25a8528677bee4a51304ff521d2ffa
Lucas Monteiro Inacio <lmsupport@outlook.com>
2022-06-07 22:08:51 -0300

    :art:: (config) http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalHttpExceptionFilter } from './core/filters/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalHttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
