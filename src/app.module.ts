a034374c29668dbfaefc2e122d86e44b83c36e8a
Lucas Monteiro Inacio <lmsupport@outlook.com>
2022-06-10 16:55:14 -0300

    :sparkles:: (config) winston logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmService } from './config';
+import { WinstonModule } from 'nest-winston';
+import { TypeOrmService, WinstonConfigService } from './config';
 import { HealthModule } from './health/health.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
   imports: [
+    WinstonModule.forRootAsync({
+      useClass: WinstonConfigService,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmService,
     }),
@@ -16,5 +20,6 @@ import { TasksModule } from './modules/tasks/tasks.module';
     HealthModule,
     TasksModule,
   ],
+  providers: [Logger],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston/dist/winston.constants';
 import { AppModule } from './app.module';
 import { GlobalHttpExceptionFilter } from './core/filters/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
 
   await app.listen(process.env.PORT || 3000);
