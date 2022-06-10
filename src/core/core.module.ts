11e44c1f67c80e69723fbeb1b0ec6704e2d87422
Lucas Monteiro Inacio <lmsupport@outlook.com>
2022-06-10 19:26:07 -0300

    :recycle:: (config) core module

---
+++ src/core/core.module.ts
@@ -0,0 +1,24 @@
+import { WinstonConfigService, TypeOrmService } from '@config';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
+import { HealthModule } from 'src/health/health.module';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    WinstonModule.forRootAsync({
+      useClass: WinstonConfigService,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmService,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HealthModule,
+  ],
+})
+export class CoreModule {}
