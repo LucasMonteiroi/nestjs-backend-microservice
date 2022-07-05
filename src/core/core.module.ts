48b67a89784be84343766729f7bf54ae0370e4c4
Lucas Monteiro Inacio <lmsupport@outlook.com>
2022-07-05 15:52:02 -0300

    :fire:: (config) removing morgan

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,11 +1,9 @@
-import { WinstonConfigService, TypeOrmService } from '@config';
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { WinstonConfigService, TypeOrmService } from '../config';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WinstonModule } from 'nest-winston';
-import { MorganMiddleware } from './middlewares/morgan.middleware';
-import helmet from 'helmet';
-import { HealthModule } from '@health/health.module';
+import { HealthModule } from '../health/health.module';
 
 @Module({
   controllers: [],
@@ -23,18 +21,4 @@ import { HealthModule } from '@health/health.module';
     HealthModule,
   ],
 })
-export class CoreModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(
-        helmet({
-          hidePoweredBy: true,
-          xssFilter: true,
-          crossOriginOpenerPolicy: false,
-          crossOriginResourcePolicy: false,
-        }),
-        MorganMiddleware,
-      )
-      .forRoutes('*');
-  }
-}
+export class CoreModule {}
